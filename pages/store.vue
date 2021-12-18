efccc984c13db0dbf1638e52480fb5790ddbd914
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-12-18 08:55:36 +0100

    store

--- pages/store.vue
+++ pages/store.vue
@@ -3,13 +3,76 @@
     <v-col cols="12">
       <v-card>
         <v-card-title class="headline">
-          Assets and Static Files
+          Frontend Store
         </v-card-title>
         <v-card-text>
           <p>
-            All special svg icons, png and graphical content are present in this folder
+            - This is used to manage data that needs to be persisted <br />
+            - This is done with the help of VueX <br />
+            - The store handler can be found in <code>./src/store</code> <br />
+            - There are getters and mutations for updating and modifying store records
           <p/>
-          <p>The pictures of the entire team members used on the website can be found in the teams folder</p>
+          <hr />
+          <h3>Persisted data include:</h3>
+          <p>
+            <code>
+              {
+                user_token, <br />
+                web_token, <br />
+                user: { <br />
+                  address, <br />
+                  date_joined, <br />
+                  email, <br />
+                  firstname, <br />
+                  image_url, <br />
+                  last_login, <br />
+                  lastname, <br />
+                }, <br />
+                subscription, <br />
+                subscription_status, <br />
+                isBurgerOpened <br />
+                isLoading <br />
+                isToast, <br />
+                cartItems <br />
+                isApp <br />
+                signup: { <br />
+                  firstname, <br />
+                  lastname, <br />
+                  email, <br />
+                  mobile, <br />
+                  password, <br />
+                }, <br />
+                settings: { <br />
+                  plan: [ <br />
+                    { <br />
+                      name, <br />
+                      amount, <br />
+                    }, <br />
+                    { <br />
+                      name, <br />
+                      amount, <br />
+                    }, <br />
+                    { <br />
+                      name, <br />
+                      amount, <br />
+                    }, <br />
+                  ], <br />
+                }, <br />
+                filters: { <br />
+                  q, <br />
+                  m, <br />
+                  s, <br />
+                  t, <br />
+                  pg, <br />
+                  s_dt, <br />
+                  e_dt, <br />
+                  principle_, <br />
+                  all_, <br />
+                  case_, <br />
+                } <br />
+              } <br />
+            </code>
+          </p>
         </v-card-text>
         <v-card-actions v-if="true === false">
           <v-spacer />
