02797840b0f63fba43c4b4fb508fa584b1fc69da
Boluwaji Akinsefunmi <bolu.akinsefunmi@gmail.com>
2021-12-18 09:19:36 +0100

    glossary docs not complete but I gotta go

---
+++ pages/glossary.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title class="headline">
+          Glossary: How To's and Quick Fixes...
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Work-In-Progress!!!
+          <p/>
+        </v-card-text>
+        <v-card-actions v-if="true === false">
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
